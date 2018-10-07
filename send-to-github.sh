92e5c3e8a085575600baefdaedfbb8776ecc9c4b
cyberdonos <cyberdonos@protonmail.com>
2018-10-07 09:52:06 +0700

    обновлён

--- send-to-github.sh
+++ send-to-github.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-sudo -u cyberdonos git add .
-sudo -u cyberdonos git commit -m "$1"
- 
+function commit {
+  sudo -u cyberdonos git add .
+  sudo -u cyberdonos git commit -m "$1"
+  git push origin master
+}
+
+cd /opt/projects/cyberdonos-browser-extension
+[[ "$PWD" =~ /opt/projects/cyberdonos-browser-extension ]] && sudo chown -R cyberdonos:git-users * 
+[ ! -z "$1" ] && [[ "$PWD" =~ /opt/projects/cyberdonos-browser-extension ]] && commit $1 
